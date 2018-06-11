69a7864b1699f947494a22b808fa6e2bc61e7e22
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-06-11 12:35:30 +0200

    Add project files

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var flkty = new Flickity('.carousel', {hash: true,});
+
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray(buttons);
+
+buttonGroup.addEventListener('click', function (event) {
+    
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.select(index);
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
